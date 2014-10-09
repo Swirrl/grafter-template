3fe647b5a2eec4f209e53494be0b3a16b6a73de4
Rick Moynihan <rick.m@swirrl.com>
2014-10-09 14:51:52 +0100

    Provide some top-level functions for running the pipeline and the pipeline-and-graph-transformation at a REPL.

--- resources/leiningen/new/grafter/core.clj
+++ resources/leiningen/new/grafter/core.clj
@@ -14,15 +14,22 @@
 
     (add (ses/rdf-serializer destination) quads)))
 
+
+(defn apply-pipeline [path]
+  (-> (open-all-datasets path)
+      first
+      pipeline))
+
+(defn apply-complete-transformation [path]
+  (-> (apply-pipeline path)
+      make-graph))
+
 (defn -main [& [path output]]
   (when-not (and path output)
     (println "Usage: lein run <input-file.csv> <output-file.(nt|rdf|n3|ttl)>")
     (System/exit 0))
 
-  (-> (open-all-datasets path)
-      first
-      pipeline
-      make-graph
+  (-> (apply-complete-transformation path)
       (import-data output))
 
   (println path "=>" output))
