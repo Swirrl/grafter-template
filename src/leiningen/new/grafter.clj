01d494ecf023687b54f0fc2160d51be38b0a7d12
Rick Moynihan <rick.m@swirrl.com>
2015-01-19 18:44:37 +0000

    Include both graft/pipe in example output

--- src/leiningen/new/grafter.clj
+++ src/leiningen/new/grafter.clj
@@ -30,7 +30,9 @@ To run the example pipeline defined in this project run:
 
 $ cd " name "
 
-$ lein grafter run " name ".pipeline/convert-persons-data ./data/example-data.csv example-output.ttl
+$ lein grafter run " name ".pipeline/convert-persons-data-to-graph ./data/example-data.csv example-output.ttl
+
+$ lein grafter run " name ".pipeline/convert-persons-data ./data/example-data.csv example-output.xls
 
 "))
     (->files data
