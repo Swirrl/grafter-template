54f3e055bb3fe8472cfd23fec030914213adc81a
Rick Moynihan <rick.m@swirrl.com>
2015-05-27 10:23:05 +0100

    Update to grafter 0.5.0-SNAPSHOT

--- resources/leiningen/new/grafter/project.clj
+++ resources/leiningen/new/grafter/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [grafter "0.4.0"]
+                 [grafter "0.5.0-SNAPSHOT"]
                  [grafter/vocabularies "0.1.0"]
                  [org.slf4j/slf4j-jdk14 "1.7.5"]]
 
@@ -18,7 +18,7 @@
                        ;;"-Xmx4g"
                        ]
 
-  :plugins [[lein-grafter "0.4.0"]]
+  :plugins [[lein-grafter "0.5.0-SNAPSHOT"]]
 
 
   )
