4439b5eace86a0ce2ad5ba278b00a9298926da42
Rick Moynihan <rick.m@swirrl.com>
2015-07-29 15:39:48 +0100

    Update template version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/lein-template "0.5.1"
+(defproject grafter/lein-template "0.5.2"
   :description "Leiningen template for a simple grafter project"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
