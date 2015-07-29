790e11cf622aff51e135c2e303db9f174e511f07
Rick Moynihan <rick.m@swirrl.com>
2015-07-29 15:39:18 +0100

    Update to latest grafter vocabularies

--- resources/leiningen/new/grafter/project.clj
+++ resources/leiningen/new/grafter/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [grafter "0.5.0"]
-                 [grafter/vocabularies "0.1.0"]
+                 [grafter/vocabularies "0.1.2"]
                  [org.slf4j/slf4j-jdk14 "1.7.5"]]
 
   :repl-options {:init (set! *print-length* 200)
