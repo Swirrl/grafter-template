a17e93478365cca4a55d3282a12da7abaa5a2d26
Rick Moynihan <rick.m@swirrl.com>
2016-08-31 10:23:40 +0100

    Version 0.7.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/lein-template "0.7.5-SNAPSHOT"
+(defproject grafter/lein-template "0.7.5"
   :description "Leiningen template for a simple grafter project"
   :url "http://grafter.org/"
   :license {:name "Eclipse Public License"
